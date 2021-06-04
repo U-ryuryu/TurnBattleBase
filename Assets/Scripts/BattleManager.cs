3588f05cbfa5fd5379b2e85709b5c4b131d30956
U-ryuryu <kugyuryuryu@gmail.com>
2021-06-04 16:05:43 +0900

    playerとenemyのターンを実装

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -8,9 +8,16 @@ public class BattleManager : MonoBehaviour
     public UnitManager player;
     public UnitManager enemy;
 
-    void Start()
+    // PlayerがEnemyに攻撃する
+    public void OnAttackButton()
     {
         player.Attack(enemy);
+        EnemyTurn();
+    }
+
+    // EnemyがPlayerに攻撃する
+    void EnemyTurn()
+    {
         enemy.Attack(player);
     }
 }
